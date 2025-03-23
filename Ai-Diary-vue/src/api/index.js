889a31452eedff72ce317bd1f8d4b78a378a65aa
feed-mina <myelin24@naver.com>
2025-03-23 09:12:55 +0900

    api 주소 임시주소-ã - server address 로 바꿈

--- Ai-Diary-vue/src/api/index.js
+++ Ai-Diary-vue/src/api/index.js
@@ -1,1 +1,1 @@
-export const apiUrl = window.__ENV__?.VUE_APP_API_BASE_URL || "https://default-url.com/api";
+export const apiUrl = window.__ENV__?.VUE_APP_API_BASE_URL || "https://justsaying.co.kr/api";

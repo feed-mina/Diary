55fbe69ef07a4c35280894a2471c53925c6458a0
feed-mina <myelin24@naver.com>
2026-01-14 08:15:11 +0900

    teãUitest: Meta로직 ë테스트 및 Diary 테스트 에러 수정 20260114

--- Ai-Diary-server/src/test/java/com/domain/demo_backend/UiControllerTest.java
+++ Ai-Diary-server/src/test/java/com/domain/demo_backend/UiControllerTest.java
@@ -1,4 +1,31 @@
 package com.domain.demo_backend;
 
+import com.domain.demo_backend.controller.UiController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("로그인 페이지 메타데이터를 성공적으로 가져오는 테스트")
+    void getLoginUiMetadataTest() throws Exception{
+        mockMvc.perform(get("/api/ui/LOGIN_PAGE"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("success")) // 응답구조확인
+                .andExpect(jsonPath("$.data[0].screenId").value("LOGIN_PAGE"))
+                .andExpect(jsonPath("$.data.length()").isNotEmpty());
+    }
 }

13d26f9da48fb25bd15ad9bbf8c0a00ec9bd4fc3
feed-mina <myelin24@naver.com>
2026-01-15 19:46:52 +0900

    에fix:빌드 중 ã에러 난 부분 수정

--- Ai-Diary-server/src/main/java/com/domain/demo_backend/query/controller/CommonQueryController.java
+++ Ai-Diary-server/src/main/java/com/domain/demo_backend/query/controller/CommonQueryController.java
@@ -5,6 +5,7 @@ import com.domain.demo_backend.query.repository.DynamicExecutor;
 import com.domain.demo_backend.query.service.QueryMasterService;
 import com.domain.demo_backend.util.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -29,7 +30,9 @@ public class CommonQueryController {
         System.out.println("@@@ 공통 실행기 진입 sqlkey: " + sqlKey);
         // 1. 설계도 전체 (Entity)를 가져온다
         QueryMaster queryMaster = queryMasterService.getQueryInfo(sqlKey);
-
+        if(queryMaster == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", "등록되지 않은 SQL 키 입니다"));
+        }
        // todo : 동적 쿼리 실행기 (DynamicExecutor) 호출 로직이 들어간다
         // 서비스 로부터 SQL 설계도(Query text)를 가져온다. (Redis 또는 DB)
 //        String query = queryMasterService.getQuery(sqlKey);
@@ -50,28 +53,31 @@ public class CommonQueryController {
 
         // returnType에 따른 분기 처리
         Object result;
-        if("COMMAND".equals(returnType)){
-            // INSERT, UPDATE, DELETE인 경우
-            result = dynamicExecutor.executeUpdate(query, params);
-        } else {
-            // SELECT인 경우 (SINGLE, MULTI등)
-            List<Map<String, Object>> list = dynamicExecutor.executeList(query, params);
-
-            if ("SINGLE".equals(returnType)) {
-                //
-                if (list != null && !list.isEmpty()) {
-                    result = list.get(0);
+            try{
+                if("COMMAND".equals(returnType)){
+                    // INSERT, UPDATE, DELETE인 경우
+                    result = dynamicExecutor.executeUpdate(query, params);
                 } else {
-                    // 결과가 없으면 null 혹은 빈 객체 반환
-                    result = null;
-                }
-            }else{
-                // MULTI 타입인 경우 리스트 통째로 반환
-                result = list;
-            }
+             // SELECT인 경우 (SINGLE, MULTI등)
+                List<Map<String, Object>> list = dynamicExecutor.executeList(query, params);
 
-            result = dynamicExecutor.executeList(query, params);
-        }
+                if ("SINGLE".equals(returnType)) {
+                    //
+                    if (list != null && !list.isEmpty()) {
+                        result = list.get(0);
+                    } else {
+                        // 결과가 없으면 null 혹은 빈 객체 반환
+                        result = null;
+                    }
+                }else{
+                    // MULTI 타입인 경우 리스트 통째로 반환
+                    result = list;
+                }
+
+            }
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("message", "쿼리 실행 중 오류가 발생했습니다.", "error", e.getMessage()));
+            }
 
         return ResponseEntity.ok().body(Map.of(
                 "status", "success", "sqlKey", sqlKey, "data", result != null ? result : Map.of()
